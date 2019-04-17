93a7015ffaf7bc862f3ab976b5dc4fac1ed652fd
ian93 <jwtaylor925@gmail.com>
2019-04-17 16:48:07 +0800

    test: Test Layout component

---
+++ src/containers/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Layout from '../components/Layout/Layout'
+
+class App extends Component {
+  render() {
+    return(
+      <Layout>
+        <p>Test Layout</p>
+      </Layout>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'Burger Builder';
+import App from './containers/App'
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <App />,
   document.getElementById('App')
 );
 
